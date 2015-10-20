9bf38ca829ae04cfe8ee7a2c8ce886b0d053298c
Joumana <JoumanaE@users.noreply.github.com>
2015-10-20 13:01:43 -0400

    Update address_book.rb

--- models/address_book.rb
+++ models/address_book.rb
@@ -3,39 +3,6 @@ require "csv"
 
 class AddressBook
   attr_accessor :entries
-
-  def initialize
-    @entries = []
-  end
-  def add_entry(name, phone_number, email)
-    index = 0
-    @entries.each do |entry|
-      if name < entry.name
-        break
-      end
-      index +=1
-    end
-
-    @entries.insert(index, Entry.new(name, phone_number, email))
-end
-
-def import_from_csv(file_name)
-  csv_text = File.read(file_name)
-  csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
-
-  csv.each do |row|
-    row_hash = row.to_hash
-    add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
-  def remove_entry(name, phone_number, email)
-    @entries.each do |entry|
-      if entry.name == name && entry.phone_number == phone_number && entry.email == email
-        @entries.delete(entry)
-        break
-      end
-    end
-  end
-end
-
 require_relative 'entry'
 require "csv"
 
@@ -58,6 +25,20 @@ class AddressBook
   end
 
   def import_from_csv(file_name)
-    #Implementation goes here
+    csv_text = File.read(file_name)
+    csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+    csv.each do |row|
+      row_hash = row.to_hash
+      add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+    end
+  end
+
+  def email_finder(name)
+    @entries.each do |entry|
+      if entry.name == name
+        puts "The email is #{entry.email}"
+        break
+      end
+    end
   end
 end
